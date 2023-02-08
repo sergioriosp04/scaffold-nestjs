1fde2047a406514964d668d39982b240edd0eb34
sergioriosp04 <sergio.rios@payco.co>
2023-02-08 17:42:59 -0500

    feat(sequelize): install sequelize to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT) || 3306,
+      username: process.env.DATABASE_USERNAME || 'root',
+      password: process.env.DATABASE_PASSWORD || 'root',
+      database: process.env.DATABASE_NAME || 'test',
+      models: [],
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
