fa8445542a6d520de0d272f0426a264505af7911
sergioriosp04 <sergio.rios@payco.co>
2023-02-09 15:19:19 -0500

    feat(user): user controller and service method for get and get all

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpStatus, NotFoundException } from '@nestjs/common';
+import { Get, HttpCode, Param, Res } from '@nestjs/common/decorators';
+import { UsersService } from './users.service';
+import { Response } from 'express';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll()
+  }
+
+  @Get(':id')
+  @HttpCode(404)
+  async findOne(@Param('id') id: string) {
+    const user = await this.userService.findOne(id)
+    if (!user) {
+      throw new NotFoundException('not user found')
+    }
+    
+    return user
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.model';
+import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User)
+    private userModel: typeof User,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.userModel.findAll()
+  }
+
+  findOne(id: string): Promise<User> {
+    return this.userModel.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+}
